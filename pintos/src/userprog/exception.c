c49b47f715f514bcb745aa1ccb2a0fad1c6b499c
inosphe <inosphe@gmail.com>
2015-04-03 20:05:18 +0900

    remove log

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -83,7 +83,7 @@ kill (struct intr_frame *f)
   /* The interrupt frame's code segment value tells us where the
      exception originated. */
 
-  debug_backtrace();
+  //debug_backtrace();
   switch (f->cs)
     {
     case SEL_UCSEG:
@@ -153,11 +153,11 @@ page_fault (struct intr_frame *f)
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
+  // printf ("Page fault at %p: %s error %s page in %s context.\n",
+  //         fault_addr,
+  //         not_present ? "not present" : "rights violation",
+  //         write ? "writing" : "reading",
+  //         user ? "user" : "kernel");
   kill (f);
 }
 
