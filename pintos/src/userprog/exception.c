374c9f606ca5124649d6fda43d06da7afba0bfac
inosphe <inosphe@gmail.com>
2015-04-03 20:49:19 +0900

    exception

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -91,6 +91,7 @@ kill (struct intr_frame *f)
       printf ("%s: dying due to interrupt %#04x (%s).\n",
               thread_name (), f->vec_no, intr_name (f->vec_no));
       intr_dump_frame (f);
+      break;
 
     case SEL_KCSEG:
       /* Kernel's code segment, which indicates a kernel bug.
@@ -100,13 +101,16 @@ kill (struct intr_frame *f)
       intr_dump_frame (f);
       debug_backtrace();
       PANIC ("Kernel bug - unexpected interrupt in kernel"); 
+      break;
 
     default:
       /* Some other code segment?  Shouldn't happen.  Panic the
          kernel. */
       printf ("Interrupt %#04x (%s) in unknown segment %04x\n",
              f->vec_no, intr_name (f->vec_no), f->cs);
+      break;
     }
+    
     exit(-1);
 }
 
