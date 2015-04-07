4b4b3536711236dd040061f7d6ab198e4973c8cf
inosphe <inosphe@gmail.com>
2015-04-07 16:20:24 +0900

    test

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -42,13 +42,12 @@ syscall_handler (struct intr_frame *f)
   #define DECL_ARGS(count) i = count-1; get_argument (esp, &arg, count);
 
   int *arg = 0;
-  int *esp = 0;
+  void *esp = 0;
   int number;
   int i = 0;
   esp = f->esp;
   check_address (esp);
-  //number = *(int*)esp;
-  number = *esp;
+  number = *(int*)esp;
   /* systemcall number is located in the top of user stack */
   esp += 4;
   switch (number)
